bae75c177ba300a1ebecddcfa1ef2e5ba4025bf1
Guillaume Meunier <guillaume.meunier@centraliens.net>
2015-08-22 15:23:12 +0200

    Update web interface

--- SampleWebUI/script.js
+++ SampleWebUI/script.js
@@ -66,7 +66,7 @@ RemoteEverything.prototype.refresh = function()
 
             that.data = newdata;
 
-            var values = document.getElementsByClassName("data-value");
+            var values = document.getElementsByClassName("data-field");
             for(var i = 0; i < values.length; i++)
             {
                 var id = values[i].dataset.logicalId;
@@ -74,7 +74,60 @@ RemoteEverything.prototype.refresh = function()
                 var field = values[i].dataset.field;
                 $(values[i]).empty();
                 if (that.data[id][type][field].value !== undefined)
-                    values[i].appendChild(document.createTextNode(that.data[id][type][field].value));
+                {
+                    var value = that.data[id][type][field].value;
+                    var unit = that.data[id][type][field].unit || "";
+                    var mult = 0;
+
+                    if (Math.abs(value) >= 1000)
+                    {
+                        while (Math.abs(value) >= 1000 && mult < 3)
+                        {
+                            value /= 1000;
+                            mult++;
+                        }
+                    }
+                    else if (Math.abs(value) < 1 && value != 0)
+                    {
+                        while (Math.abs(value) < 1 && mult > -3)
+                        {
+                            value *= 1000;
+                            mult--;
+                        }
+                    }
+
+                    switch(mult)
+                    {
+                        case 1:
+                            unit = "k" + unit;
+                            break;
+                        case 2:
+                            unit = "M" + unit;
+                            break;
+                        case 3:
+                            unit = "G" + unit;
+                            break;
+                        case -1:
+                            unit = "m" + unit;
+                            break;
+                        case -2:
+                            unit = "µ" + unit;
+                            break;
+                        case -3:
+                            unit = "n" + unit;
+                            break;
+                    }
+
+                    var valueSpan = document.createElement("span");
+                    valueSpan.appendChild(document.createTextNode(value.toFixed(2)));
+                    valueSpan.classList.add("data-value");
+                    values[i].appendChild(valueSpan);
+
+                    var unitSpan = document.createElement("span");
+                    unitSpan.appendChild(document.createTextNode(unit));
+                    unitSpan.classList.add("data-value-unit");
+                    values[i].appendChild(unitSpan);
+                }
             }
 
             var tables = document.getElementsByClassName("data-table");
@@ -102,41 +155,27 @@ RemoteEverything.prototype.createWindow = function(id)
     var w = new Window(document.body, "Data for " + id);
     w.element.dataset.logicalId = id;
     
+    var newItemDropdown = w.element.getElementsByClassName("new-item-selection")[0];
     
     for(var type in this.data[id])
     {
         for(var field in this.data[id][type])
         {
-            var tr = document.createElement("tr");
-            var td1 = document.createElement("td");
-            var td2 = document.createElement("td");
-            var td3 = document.createElement("td");
+            var displayName = field;
 
-            td1.classList.add("delete-field");
-            td1.appendChild(document.createTextNode("D"));
-            td1.onclick = function(tbody, tr, w)
-            {
-                return function()
-                {
-                    tbody.removeChild(tr);
-                    w.save();
-                };
-            }(w.tbody, tr, w);
-            
-            if (this.data[id][type][field].displayName === undefined)
-                td2.appendChild(document.createTextNode(field));
-            else
-                td2.appendChild(document.createTextNode(this.data[id][type][field].displayName));
-            
-            td3.classList.add("data-value");
-            td3.dataset.logicalId = id;
-            td3.dataset.type = type;
-            td3.dataset.field = field;
+            if (this.data[id][type][field].displayName !== undefined)
+                displayName = this.data[id][type][field].displayName;
+            dataset = { id: id, type: type, field: field, displayName: displayName };
 
-            tr.appendChild(td1);
-            tr.appendChild(td2);
-            tr.appendChild(td3);
-            w.tbody.appendChild(tr);
+            w.addField(dataset);
+
+            var opt = document.createElement("option");
+            opt.appendChild(document.createTextNode(displayName));
+            opt.dataset.id = id;
+            opt.dataset.type = type;
+            opt.dataset.field = field;
+            opt.dataset.displayName = displayName;
+            newItemDropdown.appendChild(opt);
         }
     }
 }
@@ -144,28 +183,33 @@ RemoteEverything.prototype.createWindow = function(id)
 function Window(parent, title)
 {
     var that = this;
-    
+
     this.title = title;
     this.element = document.createElement("div");
     this.element.classList.add("window", "data-window");
-    
+
     this.element.innerHTML = '<h1>' +
         '<span class="window-title"></span>' +
         '<span class="close-button">X</span>' +
         '<span class="edit-button">E</span>' +
         '</h1>' +
-        '<div class="window-content"><table><tbody class="data-table"></tbody></table></div>';
+        '<div class="window-content">' +
+        '<table><tbody class="data-table"></tbody></table>' +
+        '<select class="new-item-selection"></select><button class="new-item-button">+</button>'
+        '</div>';
 
     this.element.getElementsByClassName("window-title")[0].appendChild(document.createTextNode(title));
     this.element.getElementsByClassName("close-button")[0].onclick = function() { that.close(); };
     this.element.getElementsByClassName("edit-button")[0].onclick = function() { that.edit(); };
+    this.element.getElementsByClassName("new-item-button")[0].onclick = function() { that.addField(that.element.getElementsByClassName("new-item-selection")[0].selectedOptions[0].dataset); };
+
     parent.appendChild(this.element);
-    
+
     this.content = this.element.getElementsByClassName("window-content")[0];
     this.tbody = this.element.getElementsByTagName("tbody")[0];
     $(this.tbody).sortable({axis: "y"}).sortable("disable");
     $(this.tbody).disableSelection();
-    
+
     $(this.element).draggable({ handle: "h1", stack: ".window" });
     $(this.element).on("sortupdate", function() { that.save(); });
 }
@@ -188,6 +232,40 @@ Window.prototype.save = function()
     console.log("save");
     // TODO: enregistrer dans localStorage
 }
+
+Window.prototype.addField = function(dataset)
+{
+    var tr = document.createElement("tr");
+    var td1 = document.createElement("td");
+    var td2 = document.createElement("td");
+    var td3 = document.createElement("td");
+
+    td1.classList.add("delete-field");
+    td1.appendChild(document.createTextNode("D"));
+    td1.onclick = function(tbody, tr, w)
+    {
+        return function()
+        {
+            tbody.removeChild(tr);
+            w.save();
+        };
+    }(this.tbody, tr, this);
+
+    var displayName = dataset.displayName;
+
+    td2.appendChild(document.createTextNode(displayName));
+
+    td3.classList.add("data-field");
+    td3.dataset.logicalId = dataset.id;
+    td3.dataset.type = dataset.type;
+    td3.dataset.field = dataset.field;
+
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    this.tbody.appendChild(tr);
+}
+
 
 Window.prototype.close = function()
 {
